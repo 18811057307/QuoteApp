80e7b37fa1d83bb89f9a7fc00829a2e97ff50704
guo.tao.qin <18811057307@139.com>
2017-09-22 13:20:54 +0800

    add company

--- src/main/resources/static/app/controller/SysUserController.js
+++ src/main/resources/static/app/controller/SysUserController.js
@@ -45,6 +45,52 @@ Ext.define('tms.controller.SysUserController', {
                     scope:this
                 }
             },
+            'companyList':{
+                selectionchange:{
+                    fn:this.onCompanySelectionChange,
+                    scope:this
+                },
+                itemdblclick:{
+                    fn:this.onCompanyItemDblClick,
+                    scope:this
+                }
+            },
+            'companyList button[action=create]':{
+                click:{
+                    fn:this.onCompanyAdd,
+                    scope:this
+                }
+            },
+            'companyList button[action=update]':{
+                click:{
+                    fn:this.onCompanyEdit,
+                    scope:this
+                }
+            },
+            'companyList button[action=delete]':{
+                click:{
+                    fn:this.onCompanyDelete,
+                    scope:this
+                }
+            },
+            'companyList button[action=refresh]':{
+                click:{
+                    fn:this.onCompanyRefresh,
+                    scope:this
+                }
+            },
+            'companyUpdate button[action=submitForm]':{
+                click:{
+                    fn:this.onCompanySubmit,
+                    scope:this
+                }
+            },
+            'companyUpdate button[action=cancelForm]':{
+                click:{
+                    fn:this.onCompanyCancel,
+                    scope:this
+                }
+            },
             'sysUserUpdate button[action=submitForm]':{
                 click:{
                     fn:this.onSubmit,
@@ -86,6 +132,12 @@ Ext.define('tms.controller.SysUserController', {
         }
         
     },
+    onCompanySelectionChange:function (me, e, eOpts) {
+        var editButton = Ext.ComponentQuery.query('companyList button[action=update]')[0];
+        var deleteButton = Ext.ComponentQuery.query('companyList button[action=delete]')[0];
+        editButton.enable();
+        deleteButton.enable();
+    },
     onRoleCheckChange:function( node, checked, eOpts) {
         var userList = Ext.ComponentQuery.query('sysUserList')[0];
         var userRec = userList.getSelectionModel().getSelection()[0];
@@ -107,6 +159,9 @@ Ext.define('tms.controller.SysUserController', {
     },
     onItemDblClick:function (me, e, eOpts) {
         this.onEdit();
+    },
+    onCompanyItemDblClick:function (me, e, eOpts) {
+        this.onCompanyEdit();
     },
 
     onAdd:function (me, e, eOpts) {
@@ -131,6 +186,9 @@ Ext.define('tms.controller.SysUserController', {
     _getStore:function () {
         return  Ext.data.StoreManager.lookup('SysUserStore');
     },
+    _getCompanyStore:function () {
+        return  Ext.data.StoreManager.lookup('CompanyStore');
+    },
     onDelete:function (me, e, eOpts) {
         var grid = Ext.ComponentQuery.query('sysUserList')[0];
         var record = grid.getSelectionModel().getSelection()[0];
@@ -146,7 +204,26 @@ Ext.define('tms.controller.SysUserController', {
 
     },
     onRefresh:function (me, e, eOpts) {
-        this._getStore().load();
+    	var grid = Ext.ComponentQuery.query('sysUserList')[0];
+        var record = grid.getSelectionModel().getSelection()[0];
+        Ext.MessageBox.confirm(
+        	i18n.t('small_hint')
+            , Ext.String.format('确认重置选中的{0}的密码吗？', i18n.t('sysUser') + ":" + record.getRecordName())
+            , function (btn) {
+                if (btn == "yes") {
+                    Ext.Ajax.request({
+                        url: tms.getContextPath() + 'api/sysUser/resetPassword',
+                        params: {
+                            loginName: record.get("loginName")
+                        },
+                        success: function(response) {
+                            var data = Ext.JSON.decode(response.responseText);
+                            tms.notify(data.message, record.getRecordName());	
+                        },
+                        scope:this
+                    });
+                }
+            }, this);
     },
     onSubmit:function (me, e, eOpts) {
         var formPanel = Ext.ComponentQuery.query('sysUserForm')[0];
@@ -181,10 +258,82 @@ Ext.define('tms.controller.SysUserController', {
     },
     _closeWin:function () {
         var win = Ext.ComponentQuery.query('sysUserUpdate')[0];
+        
         win.close();
     },
     onCancel:function (me, e, eOpts) {
         this._closeWin();
+    },
+    _openCompanyUpdateWin:function (record) {
+        var updateWin = Ext.create('tms.view.company.Update');
+        updateWin.width = Ext.getBody().getViewSize().width * 0.6;
+        updateWin.height = Ext.getBody().getViewSize().height * 0.6;
+        var form = Ext.ComponentQuery.query('companyForm')[0];
+        form.loadRecord(record);
+        updateWin._setTitle(record);
+        updateWin.show();
+        this._curRecord = record;
+    },
+    onCompanyAdd:function (me, e, eOpts) {
+        var record = Ext.create('tms.model.Company');
+        this._openCompanyUpdateWin(record);
+    },
+    onCompanyEdit:function (me, e, eOpts) {
+        var grid = Ext.ComponentQuery.query('companyList')[0];
+        var record = grid.getSelectionModel().getSelection()[0];
+        this._openCompanyUpdateWin(record);
+    },
+    onCompanyDelete:function (me, e, eOpts) {
+        var grid = Ext.ComponentQuery.query('companyList')[0];
+        var record = grid.getSelectionModel().getSelection()[0];
+        Ext.MessageBox.confirm(
+            i18n.t('small_hint'),
+            Ext.String.format(i18n.t('records_delete_confirm'), i18n.t('company') + ":" + record.getRecordName()),
+            function (btn) {
+                if (btn == "yes") {
+                	this._getCompanyStore().remove(record);
+                	this._getCompanyStore().sync();
+                }
+            }, this);
+
+    },
+    onCompanyRefresh:function (me, e, eOpts) {
+    	Ext.data.StoreManager.lookup('CompanyStore').load();
+    },
+    onCompanySubmit:function (me, e, eOpts) {
+        var formPanel = Ext.ComponentQuery.query('companyForm')[0];
+        formPanel.getForm().updateRecord(this._curRecord);
+        if (formPanel.getForm().isValid()) {
+            if (this._curRecord.phantom) {
+            	this._getCompanyStore().insert(0, this._curRecord);
+            }
+            if (!this._curRecord.dirty) {
+                this.onCompanyCancel();
+                return;
+            }
+            this._getCompanyStore().sync({
+                success:function () {
+                	this.onCompanyCancel();
+                },
+                failure:function (form, action) {
+                    switch (action.failureType) {
+                        case Ext.form.action.Action.CLIENT_INVALID:
+                            Ext.Msg.alert(i18n.t('Failure!'), i18n.t('Check all your fields.'));
+                            break;
+                        case Ext.form.action.Action.CONNECT_FAILURE:
+                            Ext.Msg.alert(i18n.t('Failure!'), i18n.t('AJAX communication problem.'));
+                            break;
+                        case Ext.form.action.Action.SERVER_INVALID:
+                            Ext.Msg.alert(i18n.t('Failure!'), action.result.msg);
+                    }
+                },
+                scope:this
+            });
+        }
+    },
+    onCompanyCancel:function (me, e, eOpts) {
+    	 var win = Ext.ComponentQuery.query('companyUpdate')[0];
+    	 win.close();
     }
 });
 
